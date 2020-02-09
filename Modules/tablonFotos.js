bd45bf436f10b99886e959f2abf04fe9c3b2160e
ShompiFlen <shompire02@gmail.com>
2020-02-09 06:49:55 -0300

    .

--- Modules/tablonFotos.js
+++ Modules/tablonFotos.js
@@ -1,12 +1,13 @@
 const { Webhook, MessageEmbed, Message } = require('discord.js');
 module.exports = async (message = new Message(), webHook = new Webhook()) => {
-  const embed = new MessageEmbed()
-    .setColor("BLUE")
-    .setImage(message.content)
-    .setFooter(`Enviado por ${message.author.tag}`, message.author.displayAvatarURL);
-  return await webHook.send(embed).catch(error => {
+  try {
+    const embed = new MessageEmbed()
+      .setColor("BLUE")
+      .setImage(message.content)
+      .setFooter(`Enviado por ${message.author.tag}`, message.author.displayAvatarURL);
+    await webHook.send(embed);
+  } catch (error) {
+    console.log("Hubo un error con Tablon Hook:");
     console.log(error);
-    console.log("^^^^ Error en tablonFotos.js");
-  });
-
+  }
 }
