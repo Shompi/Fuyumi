900b8c752e84b9af8445ba8ba4ec59f447849931
ShompiFlen <shompire02@gmail.com>
2020-02-09 07:43:48 -0300

    AA

--- Modules/Music/Moan.js
+++ Modules/Music/Moan.js
@@ -12,14 +12,16 @@ module.exports = async (message = new Message()) => {
   if (!member.voice.channel) await message.reply("tienes que estar en un canal de voz!");
   if (!member.voice.channel.joinable) await message.reply("no puedo entrar al canal de voz!\nPor favor revisa mis permisos.");
   const connection = await member.voice.channel.join();
-  const dispatcher = connection.play('./Moans/test.mp3');
+  const dispatcher = connection.play('Modules/Music/Moans/test.mp3', {volume:0.10, bitrate:96000});
 
   dispatcher.on('end', () => {
+    console.log(`Ended`);
     Moaning[guild.id] = false;
     connection.disconnect();
-  })
+  });
+
   dispatcher.on('start', () => {
-    Moaning[guild.id] = true;
     console.log(`Started moaning on ${guild.name} canal ${guild.me.voice.channel.name}`);
-  })
+    Moaning[guild.id] = true;
+  });
 }
