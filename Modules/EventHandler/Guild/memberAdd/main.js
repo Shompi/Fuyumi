6ec35c3160344d355660d4c734dc333813fb2a36
ShompiFlen <shompire02@gmail.com>
2020-02-17 23:47:36 -0300

    fixed mispelling

--- Modules/EventHandler/Guild/memberAdd/main.js
+++ Modules/EventHandler/Guild/memberAdd/main.js
@@ -6,25 +6,25 @@ module.exports = async (member = new GuildMember(), Muki = new Client()) => {
 
   const { guild, user } = member;
   if (user.bot) return undefined;
-  const configs = database.get(guild.id);
-  if (!configs) return console.log(`Por alguna razón la guild ${guild.name} no tenia entrada de configuración. (EHandler/Guild/memberAdd)`);
+  const config = database.get(guild.id);
+  if (!config) return console.log(`Por alguna razón la guild ${guild.name} no tenia entrada de configuración. (EHandler/Guild/memberAdd)`);
 
   if (config.welcome.enabled) {
     
     //channelID should be either a valid id string or null.
-    const channel = Muki.channels.cache.get(configs.welcome.channelID);
+    const channel = Muki.channels.cache.get(config.welcome.channelID);
 
     //If channel comes undefined
-    if (!channel) return await guild.systemChannel.send(bienvenida(user, configs)).catch(err => console.log(`La guild ${guild.name} no tiene canal de bienvenida, ni canal de sistema.`));
+    if (!channel) return await guild.systemChannel.send(bienvenida(user, config)).catch(err => console.log(`La guild ${guild.name} no tiene canal de bienvenida, ni canal de sistema.`));
     
-    else return await channel.send(bienvenida(user, configs));
+    else return await channel.send(bienvenida(user, config));
 
   }
   else return undefined;
 }
 
-const bienvenida = (user = new User(), configs) => {
-  const frases = configs.welcome.joinPhrases;
+const bienvenida = (user = new User(), config) => {
+  const frases = config.welcome.joinPhrases;
   let frase = "";
   if (frases.length > 0) frase = frases[Math.floor(Math.random() * frases.length)];
 
