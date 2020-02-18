8a44151692ba5fbae28b1a48e51afe7c0349e88f
ShompiFlen <shompire02@gmail.com>
2020-02-17 22:44:26 -0300

    Added NSFW restriction inside file

--- Modules/Boorus/Kona.js
+++ Modules/Boorus/Kona.js
@@ -2,34 +2,43 @@ const fetch = require('node-fetch');
 const { MessageEmbed, Message } = require('discord.js');
 const endpoint = 'https://konachan.com/post.json?limit=40&tags=';
 const Booru = require('../../Classes/Booru');
+
+
+const noResults = new MessageEmbed()
+  .setTitle('❌ No encontré nada con los tags que ingresaste.')
+  .setDescription('Nota: No puedes hacer una búsqueda con más de 3 tags debido a limitaciones del servidor.')
+  .setColor('RED')
+
+const notNSFW = new MessageEmbed()
+  .setTitle(`🛑 ¡Alto ahí!`)
+  .setDescription(`¡Solo puedes utilizar este comando en canales **NSFW**!`)
+  .setColor("RED");
+
 module.exports = async (message = new Message()) => {
   try {
+    const { member, channel, author } = message;
+
+    if (!channel.nsfw) return await channel.send(notNSFW);
+
     let pageindex = 0;
     let content = message.content.split(" ").slice(1).join(" ");
     let query = content.replace(/\s*\+\s*/g, "+").replace(/\s+/g, "_");
     let response = Booru.KonaPost;
-    let blacklist = '+-loli+-rape+-scat'; //Tags blacklist
+    let blacklist = '+-loli+-rape'; //Tags blacklist
 
     let data = await fetch(endpoint + query + blacklist);
-    if (!data.ok) return await message.channel.send("Error al conectar con el servidor, codigo: " + data.statusCode).catch(err => console.log(err));
+    if (!data.ok) return await channel.send("Error al conectar con el servidor, codigo: " + data.statusCode).catch(err => console.log(err));
     response = await data.json();
 
-    if (response.length === 0) {
+    if (response.length === 0) return await channel.send(noResults);
 
-      const noResults = new MessageEmbed()
-        .setTitle('❌ No encontré nada con los tags que ingresaste.')
-        .setDescription('Puedes buscar un tag escribiendo\n\`muki!tag <tagAqui>\`\nPuedes agregar un** * **alfinal para intentar autocompletar la búsqueda.')
-        .setColor('RED');
-
-      return await message.channel.send(noResults);
-    }
     const embed = await showpage(response[0], message, pageindex, response.length);
-    const msg = await message.channel.send(embed);
+    const msg = await channel.send(embed);
 
     await msg.react('⬅');
     await msg.react('➡');
-    const filter = (reaction, user) => reaction.emoji.name === '⬅' || reaction.emoji.name === '➡' && user.id === message.author.id;
 
+    const filter = (reaction, user) => reaction.emoji.name === '⬅' || reaction.emoji.name === '➡' && user.id === author.id;
     await msg.createReactionCollector(filter, { time: 1000 * 60 * 3.5 })
       .on('collect', async (reaction, user) => {
         if (reaction.emoji.name == '➡') {
@@ -47,10 +56,7 @@ module.exports = async (message = new Message()) => {
         }
 
       })
-      .on('end', async () => {
-        if (message.channel.type == 'dm') throw `No se pueden quitar las reacciones en un DM. (${message.author.tag})`;
-        return await msg.reactions.removeAll();
-      })
+      .on('end', async () => await msg.reactions.removeAll());
   }
   catch (error) {
     console.log(error);
@@ -67,7 +73,7 @@ const getRating = (r) => {
 
 const showpage = async (post = Booru.KonaPost[0], message = new Message(), index, total) => {
   const newRating = getRating(post.rating);
-  const tags = post.tags.split(" ").slice(0,10).join(", ").replace(/_/g, " ");
+  const tags = post.tags.split(" ").slice(0, 10).join(", ").replace(/_/g, " ");
 
   const embed = new MessageEmbed()
     .setAuthor(`->Full Resolución<-`, null, post.jpeg_url)
--- Modules/Boorus/Yandere.js
+++ Modules/Boorus/Yandere.js
@@ -1,33 +1,38 @@
 const fetch = require('node-fetch');
 const { MessageEmbed, Message } = require('discord.js');
 const Booru = require('../../Classes/Booru')
+
+const noResults = new MessageEmbed()
+  .setTitle('❌ No encontré nada con los tags que ingresaste.')
+  .setDescription('Nota: No puedes hacer una búsqueda con más de 3 tags debido a limitaciones del servidor.')
+  .setColor('RED')
+
+const notNSFW = new MessageEmbed()
+  .setTitle(`🛑 ¡Alto ahí!`)
+  .setDescription(`¡Solo puedes utilizar este comando en canales **NSFW**!`)
+  .setColor("RED");
+
 module.exports = async (message = new Message()) => {
   try {
+    if (!message.channel.nsfw) return await message.channel.send(notNSFW);
+
     let content = message.content.split(" ").slice(1).join(" ");
 
     if (content.split(/\s*\+\s*/g).length >= 3) return await message.reply('lo siento, solo puedes juntar máximo 3 tags para la búsqueda.');
+    
     let query = content.replace(/\s*\+\s*/g, "+").replace(/\s+/g, "_");
-    let blacklist = '+-loli+-scat'; //Tags blacklist
+    let blacklist = '+-loli'; //Tags blacklist
     let response = Booru.YanderePost;
 
     console.log(query);
     let data = await fetch('https://yande.re/post.json?limit=100&tags=' + query + blacklist);
     if (data.status != 200) {
       await message.channel.send("Error al conectar con el servidor, codigo: " + data.status);
-      console.log(data);
-      return;
+      return console.log(data);
     }
     response = await data.json();
 
-    if (response.length === 0) {
-
-      const noResults = new MessageEmbed()
-        .setTitle('❌ No encontré nada con los tags que ingresaste.')
-        .setDescription('Puedes buscar un tag escribiendo:\n\`muki!tag <tagAqui>\`\n\nPuedes agregar un** * **alfinal para intentar autocompletar la búsqueda.')
-        .setColor('RED');
-
-      return await message.channel.send(noResults);
-    }
+    if (response.length === 0) return await message.channel.send(noResults);
 
     let pageindex = Math.floor(Math.random() * response.length);
     const embed = await showpage(response[pageindex], message, pageindex, response.length);
@@ -37,7 +42,7 @@ module.exports = async (message = new Message()) => {
     await msg.react('➡');
     await msg.react('🔄');
     const filter = (reaction, user) => ((reaction.emoji.name === '⬅' || reaction.emoji.name === '➡' || reaction.emoji.name === '🔄') && user.id === message.author.id);
-  
+
     await msg.createReactionCollector(filter, { time: 1000 * 60 * 4 })
       .on('collect', async (reaction, user) => {
         if (reaction.emoji.name == '➡') {
@@ -61,10 +66,7 @@ module.exports = async (message = new Message()) => {
         }
 
       })
-      .on('end', async () => {
-        if (message.channel.type == 'dm') throw `No se pueden quitar las reacciones en un DM. (${message.author.tag})`;
-        return await msg.reactions.removeAll();
-      })
+      .on('end', async () => await msg.reactions.removeAll());
 
   }
   catch (error) {
@@ -82,7 +84,7 @@ const getRating = (r) => {
 
 const showpage = async (post = Booru.YanderePost[0], message = new Message(), index, total) => {
   const newRating = getRating(post.rating);
-  const tags = post.tags.split(" ").slice(0,10).join(", ").replace(/_/g, " ");
+  const tags = post.tags.split(" ").slice(0, 10).join(", ").replace(/_/g, " ");
   const embed = new MessageEmbed()
     .setAuthor(`->Full Resolución<-`, null, post.file_url)
     .setDescription(`**Resolución:** ${post.sample_width}x${post.sample_height} **Rating:** ${newRating}\n**Tags:** ${tags}`)
