6c5d8e0dd5abd53c059cbcce2679f16e24395cc4
ShompiFlen <shompire02@gmail.com>
2020-02-22 03:30:49 -0300

    changed login token to stable

--- Muki.js
+++ Muki.js
@@ -6,7 +6,7 @@ Muki.EventHandlers = require('./Commands/EventHandlers');
 Muki.NASA = require('./Commands/NASA/POTD');
 const fs = require('fs');
 const commandFiles = fs.readdirSync('./Commands/Commands').filter(file => file.endsWith(".js"));
-const auth = require('./Keys/auth').mukiDev;
+const auth = require('./Keys/auth').stable;
 
 for (const file of commandFiles) {
   const command = require(`./Commands/Commands/${file}`);
