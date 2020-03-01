07fbb231d067f7b884466475416af3c88f695454
ShompiFlen <shompire02@gmail.com>
2020-03-01 20:06:57 -0300

    p

--- Commands/Commands/Kick.js
+++ Commands/Commands/Kick.js
@@ -45,8 +45,8 @@ module.exports = {
 
     if (!target) return await channel.send(noTarget);
     if (!target.kickable) return await channel.send(noPermissions);
-    
-    const reason = args.shift().join(" ");
+
+    const reason = args.slice(1).join(" ");
 
     const kicked = await target.kick(reason);
 
