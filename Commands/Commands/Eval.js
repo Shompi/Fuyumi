5619529bba16ae79d13c6e182105f3c6aa5f9ec7
ShompiFlen <shompire02@gmail.com>
2020-03-15 04:22:56 -0300

    await the evaluation of code

--- Commands/Commands/Eval.js
+++ Commands/Commands/Eval.js
@@ -25,14 +25,14 @@ module.exports = {
 
     try {
       const code = args.join(" ");
-      let evaled = eval(code);
+      let evaled = await eval(code);
 
       if (typeof evaled !== "string")
         evaled = require("util").inspect(evaled);
 
-      return await channel.send(clean(evaled), { code: "xl" });
+      return channel.send(clean(evaled), { code: "js" });
     } catch (err) {
-      return await channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      return channel.send(`\`ERROR\` \`\`\`js\n${clean(err)}\n\`\`\``);
     }
 
   }
