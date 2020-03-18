07c3d90529a4444da53ee4949b12f408f8137ab7
ShompiFlen <shompire02@gmail.com>
2020-03-18 02:29:01 -0300

    Client constructor

---
+++ Classes/MukiClient.js
@@ -0,0 +1,21 @@
+const { Client, Collection } = require('discord.js');
+module.exports = class extends Client {
+  constructor(options) {
+    super({
+      disableMentions: 'everyone',
+      partials: ["GUILD_MEMBER"]
+    });
+
+    this.commands = new Collection();
+
+    this.events = new Collection();
+
+    this.OWNER = '166263335220805634';
+
+    this.Messages = new Collection();
+
+    this.eventhandler = require('../Commands/EventHandlers');
+
+    this.config = options;
+  }
+}
