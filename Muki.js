9ac7685b96b92acf97157f4c55d5a803ad9f520c
ShompiFlen <shompire02@gmail.com>
2020-03-27 22:02:03 -0300

    Token ..

--- Muki.js
+++ Muki.js
@@ -1,7 +1,7 @@
 /*----------------------MODULOS PRINCIPALES---------------------------*/
 const { MessageEmbed, Webhook } = require('discord.js');
 const MukiClient = require('./Classes/MukiClient');
-const auth = require('./Keys/auth').beta;
+const auth = require('./Keys/auth').stable;
 const fs = require('fs');
 const GuildConfig = require('./Classes/GuildConfig');
 
