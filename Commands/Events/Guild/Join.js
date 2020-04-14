62258e34413131ebac8d53d8945460e1f50afea9
ShompiFlen <shompire02@gmail.com>
2020-04-14 19:50:32 -0400

    Added more description

--- Commands/Events/Guild/Join.js
+++ Commands/Events/Guild/Join.js
@@ -2,6 +2,6 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = new MessageEmbed()
   .setTitle(`¡Gracias por añadirme al servidor!`)
-  .setDescription("Mi prefijo es `muki!`, puedes ver mi lista de comandos escribiendo `muki!help`.\n¡Asegúrate de tener activados los mensajes directos!")
+  .setDescription("Mi prefijo es `muki!`, puedes ver mi lista de comandos escribiendo `muki!help`.\n¡Asegúrate de tener activados los mensajes directos!\n\n**Nota:** Todos los comandos tienen un `cooldown` mínimo de 2 segundos, el bot **no responderá** a tus comandos si te encuentras bajo ese periodo de tiempo.")
   .setColor("BLUE")
   .setFooter("¿Dudas? ¿Sugerencias? Habla con ShompiFlen#3338");
