2037d11b4cf199443ae28584c73e768583593710
ShompiFlen <shompire02@gmail.com>
2020-04-15 22:28:35 -0400

    Now you are able to type in multiple command names and try to restart them all at once.

--- Commands/Commands/ReloadCMD.js
+++ Commands/Commands/ReloadCMD.js
@@ -1,23 +1,11 @@
 const { MessageEmbed, Message } = require('discord.js');
 const path = require('path');
 
-const noCommandFound = (author) =>
-  new MessageEmbed()
-    .setTitle(`🔎 Error: 404`)
-    .setDescription(`${author}, no he encontrado ese comando.`)
-    .setColor("YELLOW");
-
 const noCommandSpecified = (author) =>
   new MessageEmbed()
     .setTitle(`💢 Comando no especificado.`)
-    .setDescription(`${author}, ¡debes especificar un comando para recargar!`)
+    .setDescription(`${author}, ¡debes especificar al menos un comando para recargar!`)
     .setColor("RED")
-
-
-const success = (command) =>
-  new MessageEmbed()
-    .setTitle(`✅ ¡El comando '${command.name.toUpperCase()}'  se ha reiniciado correctamente!`)
-    .setColor("GREEN");
 
 module.exports = {
   //This command should be Bot OWNER only.
@@ -37,20 +25,34 @@ module.exports = {
 
     if (!args.length) return channel.send(noCommandSpecified(author));
 
-    const command = Muki.commands.get(args[0]) || Muki.commands.find(c => c.aliases.includes(args[0]));
+    const notReloaded = [], reloaded = [];
 
-    if (!command) return channel.send(noCommandFound(author));
+    args.forEach(cmd => {
+      const command = Muki.commands.get(cmd) || Muki.commands.find(c => c.aliases.includes(cmd));
 
-    //If a command is found:
-    delete require.cache[require.resolve(`./${command.filename}`)];
+      if (!command)
+        return notReloaded.push(cmd);
 
-    try {
-      const reload = require(`./${command.filename}`);
-      Muki.commands.set(reload.name, reload);
-      return channel.send(success(command));
-    } catch (error) {
-      console.log(error);
-      return channel.send("Hubo un error con la ejecución de este comando. Mira la consola.");
-    }
+      //If a command is found:
+      delete require.cache[require.resolve(`./${command.filename}`)];
+
+      try {
+        const reload = require(`./${command.filename}`);
+        Muki.commands.set(reload.name, reload);
+        return reloaded.push(reload.name);
+      } catch (error) {
+        console.log(error);
+        return notReloaded.push(cmd);
+      }
+    });
+
+    const embed = new MessageEmbed()
+      .setDescription(`**Comandos reiniciados exitósamente:**
+      \`\`\`${reloaded.join(", ")}\`\`\`
+      **Comandos no encontrados o que fallaron al reiniciar:**
+      \`\`\`${notReloaded.join(", ")}\`\`\``)
+      .setColor("BLUE");
+
+    return channel.send(embed);
   }
 }
