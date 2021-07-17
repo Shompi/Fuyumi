c960c7844094d4c740590807459999a34c3a9e23
ShompiFlen <shompire02@gmail.com>
2021-07-17 18:22:28 -0400

    Botones para el panel de roles

--- listeners/Interactions/Buttons/roles.js
+++ listeners/Interactions/Buttons/roles.js
@@ -1,5 +1,9 @@
 const { ButtonInteraction } = require("discord.js");
 
+
+const Mayor18ID = '544718986806296594';
+const ApostadorID = '745385918546051222';
+const DegeneradoID = '866061257923493918';
 /**
 @param {ButtonInteraction} interaction
  */
@@ -10,24 +14,41 @@ module.exports.AddRoles = async (interaction) => {
 	const ButtonPressed = interaction.component;
 	const RoleID = ButtonPressed.customId.slice(5);
 	const RoleObject = guild.roles.cache.get(RoleID);
+	const MemberIsOver18 = member.roles.cache.has(Mayor18ID);
+
 	let operation;
 
 	console.log(`${interaction.member.displayName} presionó ${ButtonPressed.label}`);
 
 	if (!RoleObject) return;
 
+	if ((RoleID === ApostadorID || RoleID === DegeneradoID) && !MemberIsOver18)
+		return interaction.reply({
+			content: 'Debes tener el rol **Mayor de 18** para poder asignarte este rol.',
+			ephemeral: true,
+		});
+
+
+	if (MemberIsOver18 && RoleID === Mayor18ID) {
+		await member.roles.remove([Mayor18ID, ApostadorID, DegeneradoID]);
+
+		return interaction.reply({
+			content: '**Se te han quitado todos los roles NSFW / +18**',
+			ephemeral: true,
+
+		});
+	}
+
+
 	if (member.roles.cache.has(RoleID)) {
 		operation = await member.roles.remove(RoleID).then(() => false);
-
 	}
 	else {
 		operation = await member.roles.add(RoleID).then(() => true);
 	}
 
-	interaction.reply({
-		content: `Se te ${operation ? 'agregó' : 'quitó'} el rol **${ButtonPressed.label}**`,
+	return interaction.reply({
+		content: `Se te ${operation ? 'agregó' : 'quitó'} el rol **${RoleObject.name}**`,
 		ephemeral: true,
-	})
-
-	return;
+	});
 }
--- listeners/Ready/ready.js
+++ listeners/Ready/ready.js
@@ -1,11 +1,6 @@
 const { Listener, AkairoClient } = require('discord-akairo');
 const { TextChannel, MessageButton, MessageActionRow, MessageEmbed } = require('discord.js');
 
-const activity = {
-	name: "@Muki help",
-	type: "LISTENING"
-}
-
 /**@type {NodeJS.Timeout[]} */
 const timers = [];
 
@@ -27,6 +22,12 @@ class ReadyListener extends Listener {
 				console.log("Timers limpiados.");
 			}
 		}
+
+		this.setActivity = () => {
+			timers.push(setInterval(() => {
+				this.client.user.setActivity({ name: 'Reviviendo... de a poco...', type: 'PLAYING' });
+			}, 10000));
+		}
 	}
 
 
@@ -34,14 +35,8 @@ class ReadyListener extends Listener {
 	async exec(client = this.client) {
 		/*Code Here*/
 		console.log(`Online en Discord como: ${client.user.tag}`);
+		console.log(`Bot listo: ${Date()}`);
 
-		try {
-			client.user.setPresence({ activity: activity });
-			console.log(`Bot listo: ${Date()}`);
-		} catch (error) {
-			console.log(error);
-			client.emit("error", error);
-		}
 
 		/**@type {import('discord.js').ApplicationCommandData} */
 		const data = [{
@@ -78,73 +73,56 @@ class ReadyListener extends Listener {
 					}]
 				}
 			]
-		}]
-
-		await client.guilds.cache.get("537484725896478733").commands.fetch()
-			.then(commands => commands.find(command => command.name == 'setup')?.delete());
-
-		await client.guilds.cache.get("537484725896478733").commands.set(data)
-			.then(() => console.log("El comando fue actualizado."));
-
-
+		}];
 
 		/**@type {TextChannel} */
 		const RolesChannel = client.channels.cache.get("865360481940930560");
 
+		// Roles de Juegos
 
-		// Components
-		const RocketLeagueButton = new MessageButton()
-			.setCustomId('role-585905903912615949')
-			.setLabel('Rocket League')
+		const AmongusButton = new MessageButton()
+			.setCustomId('role-750237826443903077')
+			.setLabel('Amongus')
 			.setStyle('SUCCESS');
 
-		const SeaOfThievesButton = new MessageButton()
-			.setCustomId('role-854968345974669313')
-			.setStyle('SUCCESS')
-			.setLabel('Sea of Thieves');
-
-		const GenshinImpactButton = new MessageButton()
-			.setCustomId('role-810412477116448769')
-			.setStyle('SUCCESS')
-			.setLabel('Genshin Impact');
+		const ApexLegendsButton = new MessageButton()
+			.setCustomId('role-865729353215246357')
+			.setLabel('Apex Legends')
+			.setStyle('SUCCESS');
 
 		const CSGOButton = new MessageButton()
 			.setCustomId('role-699832400510976061')
 			.setStyle('SUCCESS')
 			.setLabel('Counter-Strike: GO');
 
-		const ValorantButton = new MessageButton()
-			.setCustomId('role-707341893544968324')
-			.setLabel('Valorant')
-			.setStyle('SUCCESS');
-
-		const row1 = new MessageActionRow()
-			.addComponents([
-				RocketLeagueButton,
-				SeaOfThievesButton,
-				GenshinImpactButton,
-				CSGOButton,
-				ValorantButton
-			]);
-
-		const AmongusButton = new MessageButton()
-			.setCustomId('role-750237826443903077')
-			.setLabel('Amongus')
-			.setStyle('PRIMARY');
-
-		const TarkovButton = new MessageButton()
+		const EscapeFromTarkovButton = new MessageButton()
 			.setCustomId('role-676967872807043072')
 			.setLabel('Escape from Tarkov')
-			.setStyle('PRIMARY');
-
-		const WarzoneButton = new MessageButton()
-			.setCustomId('role-700166161295474728')
-			.setLabel('COD: Warzone')
-			.setStyle('PRIMARY');
+			.setStyle('SUCCESS');
 
 		const FortniteButton = new MessageButton()
 			.setCustomId('role-627237678248886292')
 			.setLabel('Fortnite')
+			.setStyle('SUCCESS');
+
+		const GenshinImpactButton = new MessageButton()
+			.setCustomId('role-810412477116448769')
+			.setStyle('PRIMARY')
+			.setLabel('Genshin Impact');
+
+		const LeagueButton = new MessageButton()
+			.setCustomId('role-865393189628149760')
+			.setStyle('PRIMARY')
+			.setLabel('League of Legends');
+
+		const MinecraftButton = new MessageButton()
+			.setCustomId('role-865394122897227797')
+			.setLabel('Minecraft')
+			.setStyle('PRIMARY');
+
+		const RocketLeagueButton = new MessageButton()
+			.setCustomId('role-585905903912615949')
+			.setLabel('Rocket League')
 			.setStyle('PRIMARY');
 
 		const RustButton = new MessageButton()
@@ -152,61 +130,143 @@ class ReadyListener extends Listener {
 			.setLabel('Rust')
 			.setStyle('PRIMARY');
 
-		const row2 = new MessageActionRow()
+		const SeaOfThievesButton = new MessageButton()
+			.setCustomId('role-854968345974669313')
+			.setStyle('SUCCESS')
+			.setLabel('Sea of Thieves');
+
+		const ValorantButton = new MessageButton()
+			.setCustomId('role-707341893544968324')
+			.setLabel('Valorant')
+			.setStyle('SUCCESS');
+
+		const WarzoneButton = new MessageButton()
+			.setCustomId('role-700166161295474728')
+			.setLabel('COD: Warzone')
+			.setStyle('SUCCESS');
+
+		const WarThunderButton = new MessageButton()
+			.setCustomId('role-865423770264928296')
+			.setLabel('War Thunder')
+			.setStyle('SUCCESS');
+
+		const row1 = new MessageActionRow()
 			.addComponents([
 				AmongusButton,
-				TarkovButton,
-				WarzoneButton,
+				ApexLegendsButton,
+				CSGOButton,
+				EscapeFromTarkovButton,
 				FortniteButton,
-				RustButton
 			]);
 
-		const LeagueButton = new MessageButton()
-			.setCustomId('role-865393189628149760')
-			.setStyle('SUCCESS')
-			.setLabel('League of Legends');
-
-		const MinecraftButton = new MessageButton()
-			.setCustomId('role-865394122897227797')
-			.setLabel('Minecraft')
-			.setStyle('SUCCESS');
+		const row2 = new MessageActionRow()
+			.addComponents([
+				GenshinImpactButton,
+				LeagueButton,
+				MinecraftButton,
+				RocketLeagueButton,
+				RustButton,
+			]);
 
 		const row3 = new MessageActionRow()
 			.addComponents([
-				LeagueButton,
-				MinecraftButton,
+				SeaOfThievesButton,
+				ValorantButton,
+				WarzoneButton,
+				WarThunderButton,
 			]);
 
-		const ExiliadeButton = new MessageButton()
-			.setLabel('Exiliade')
-			.setStyle('SECONDARY')
-			.setCustomId('role-539578236653404190');
+		const JuegosEmbed = new MessageEmbed()
+			.setColor('BLUE')
+			.setTitle('Roles de Juegos');
+
+		RolesChannel.messages.fetch('865370324044742656')
+			.then(message => {
+				message.edit({
+					components: [row1, row2, row3],
+					embeds: [JuegosEmbed],
+				})
+			});
+
+
+		// Roles NSFW
 
 		const AdultButton = new MessageButton()
 			.setLabel('Mayor de 18')
 			.setStyle('DANGER')
 			.setCustomId('role-544718986806296594');
 
-		const row4 = new MessageActionRow()
+		const ApostadorButton = new MessageButton()
+			.setLabel('Apostador Compulsivo')
+			.setStyle('DANGER')
+			.setCustomId('role-745385918546051222');
+
+		const DegeneradoButton = new MessageButton()
+			.setLabel('Degenerado')
+			.setStyle('DANGER')
+			.setCustomId('role-866061257923493918')
+
+		const AdultRow1 = new MessageActionRow()
 			.addComponents([
-				ExiliadeButton,
 				AdultButton,
+				ApostadorButton,
+				DegeneradoButton,
+			])
+
+		const NSFWRolesEmbed = new MessageEmbed()
+			.setTitle('Roles +18')
+			.setDescription('Si te quitas el rol **Mayor de 18** automáticamente se te quitarán todos los demás roles de esta categoria.')
+			.setColor('RED');
+
+		RolesChannel.messages.fetch("866060332594233365").then(message => {
+			message.edit({
+				embeds: [NSFWRolesEmbed],
+				components: [AdultRow1],
+			});
+		});
+
+		// Roles Misceláneos
+
+		const EstudianteButton = new MessageButton()
+			.setCustomId('role-576195996728426526')
+			.setStyle('PRIMARY')
+			.setLabel('Estudiante');
+
+		const JovenProgramadorButton = new MessageButton()
+			.setCustomId('role-690009340681388115')
+			.setStyle('PRIMARY')
+			.setLabel('Programador');
+
+		const SimpButton = new MessageButton()
+			.setCustomId('role-752006971527266374')
+			.setStyle('PRIMARY')
+			.setLabel('SIMP');
+
+		const WeebButton = new MessageButton()
+			.setCustomId('role-644251281204183070')
+			.setStyle('PRIMARY')
+			.setLabel('Weeb / Otaku');
+
+		const MiscEmbed = new MessageEmbed()
+			.setTitle('Roles Misceláneos')
+			.setDescription('No todos los roles te darán acceso a un canal nuevo, algunos son solamente para el look.')
+			.setColor('YELLOW');
+
+		const MiscRow1 = new MessageActionRow()
+			.addComponents([
+				EstudianteButton,
+				JovenProgramadorButton,
+				SimpButton,
+				WeebButton,
 			]);
 
-		const embed = new MessageEmbed()
-			.setColor('BLUE')
-			.setTitle('¡Presiona un botón para **asignarte / quitarte** un rol!');
-		RolesChannel.messages.fetch('865370324044742656')
-			.then(message => {
-				message.edit({
-					components: [row1, row2, row3, row4],
-					embeds: [embed],
-				})
+		RolesChannel.messages.fetch('866071653128732682').then(message => {
+			message.edit({
+				embeds: [MiscEmbed],
+				components: [MiscRow1],
 			});
-
-		timers.push(setInterval(() => {
-			client.user.setActivity({ name: 'Reviviendo... de a poco...', type: 'PLAYING' });
-		}, 10000));
+		});
+		this.setActivity();
 	}
 }
 
