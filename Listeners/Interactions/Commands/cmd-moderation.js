5e729a7ba851f291a7cd502310eb2ec81457434b
ShompiFlen <shompire02@gmail.com>
2022-05-20 15:01:00 -0400

    db syncs

--- Listeners/Interactions/Commands/cmd-moderation.js
+++ Listeners/Interactions/Commands/cmd-moderation.js
@@ -1,8 +1,11 @@
 const { CommandInteraction } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
 const { TimeoutMember } = require('./SubCommands/mod-timeout');
 const { Poll } = require('./SubCommands/mod-poll');
 const { Announce } = require('./SubCommands/mod-announce');
+const { UploadEmoji, UploadEmojiByURL } = require('./SubCommands/mod-uploademoji');
+
 const { ChannelType } = require('discord-api-types/v9');
 
 module.exports = {
@@ -114,7 +117,20 @@ module.exports = {
       .addStringOption(option3 => option3.setName('opcion_3').setDescription("Opcion de la encuesta"))
       .addStringOption(option4 => option4.setName('opcion_4').setDescription("Opcion de la encuesta"))
       .addStringOption(option5 => option5.setName('opcion_5').setDescription("Opcion de la encuesta"))
-      .addIntegerOption(tiempo => tiempo.setName('duracion').setDescription('La duración de esta encuesta en minutos. (def: 5 minutos)'))),
+      .addIntegerOption(tiempo => tiempo.setName('duracion').setDescription('La duración de esta encuesta en minutos. (def: 5 minutos)')))
+
+    // Upload emoji command
+    .addSubcommand(subcommand =>
+      subcommand.setName('subir_emoji')
+        .setDescription('Sube un emoji al servidor con una imagen local que puedes subir')
+        .addStringOption(nombre => nombre.setName('nombre').setDescription('El nombre para el emoji').setRequired(true))
+        .addAttachmentOption(attachment => attachment.setName('imagen').setDescription('La imágen del emoji que quieres subir').setRequired(true))
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('subir_emoji_url')
+        .setDescription('Sube un emoji al servidor con la url de una imagen')
+        .addStringOption(name => name.setName('nombre').setDescription('Nombre del emoji').setRequired(true))
+        .addStringOption(url => url.setName('imagen_url').setDescription('La URL online de la imagen del emoji'))),
   isGlobal: true,
   /**
    * 
@@ -137,6 +153,12 @@ module.exports = {
           return await Announce(interaction);
         case 'encuesta':
           return await Poll(interaction);
+        case 'subir_emoji':
+          if (!interaction.guild.me.permissions.has('MANAGE_EMOJIS_AND_STICKERS')) return await interaction.reply({ content: 'No tengo permisos para administrar emojis en este servidor. Necesito ese permiso para poder ejecutar este comando.', ephemeral: true })
+          return await UploadEmoji(interaction);
+        case 'subir_emoji_url':
+          if (!interaction.guild.me.permissions.has('MANAGE_EMOJIS_AND_STICKERS')) return await interaction.reply({ content: 'No tengo permisos para administrar emojis en este servidor. Necesito ese permiso para poder ejecutar este comando.', ephemeral: true })
+          return await UploadEmojiByURL(interaction);
       }
 
     } else {
