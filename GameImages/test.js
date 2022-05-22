955031b3274e76901f9a5edfdc1a0cdbab7f0674
Shompi <shompire02@gmail.com>
2022-05-22 07:35:46 -0400

    removed non used tests

--- GameImages/test.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios').default;
-const CREDENTIALS = require('./config.json');
-
-const url = `https://id.twitch.tv/oauth2/token?client_id=${CREDENTIALS.clientId}&client_secret=${CREDENTIALS.clientSecret}&grant_type=client_credentials`;
-axios({
-  method: 'POST',
-  url: url
-}).then(response => console.log(response));
