425c934914dbe014db08ab5a81bb9818da542071
ShompiFlen <shompire02@gmail.com>
2022-05-23 12:37:33 -0400

    image resizer command statistics

--- Listeners/Interactions/Commands/SubCommands/image-resize.js
+++ Listeners/Interactions/Commands/SubCommands/image-resize.js
@@ -1,4 +1,4 @@
-const { CommandInteraction, MessageAttachment } = require('discord.js');
+const { CommandInteraction, MessageAttachment, MessageEmbed, Util } = require('discord.js');
 const fetch = require('node-fetch').default;
 const Sharp = require('sharp');
 
@@ -7,13 +7,14 @@ const Sharp = require('sharp');
  * @param {CommandInteraction} interaction 
  */
 module.exports.ImageResize = async (interaction) => {
+
   const options = {
-    name: interaction.options.getString('nombre', false),
+    name: interaction.options.getString('nombre', false) ?? "output",
     height: interaction.options.getInteger('altura', false),
     width: interaction.options.getInteger('largo', false),
-    format: interaction.options.getString('formato', false) ?? 'png',
+    format: '',
     url: interaction.options.getAttachment('archivo', false)?.url ?? interaction.options.getString('url', false),
-    quality: interaction.options.getInteger('calidad', false)
+    quality: interaction.options.getInteger('calidad', false) ?? 80
   }
 
   if (!options.url)
@@ -31,24 +32,62 @@ module.exports.ImageResize = async (interaction) => {
       content: 'Ocurrió un error al intentar conseguir la imagen. Inténtalo más tarde.'
     });
 
+  const oldMetadata = await getImageMetadata(imageBuffer);
+  options.format = interaction.options.getString('format', false) ?? oldMetadata.format;
+
   const output = Sharp(imageBuffer)
     .resize({ height: options.height, width: options.width });
 
-  if (options.format === 'jpg')
-    output.jpeg({ quality: options.quality })
+  if (options.format === 'jpeg')
+    output.jpeg({ quality: options.quality });
   else if (options.format === 'png')
-    output.png({ quality: options.quality })
+    output.png({ quality: options.quality });
   else if (options.format === 'gif')
-    output.gif()
+    output.gif();
   else if (options.format === 'webp')
-    output.webp({ quality: options.quality })
+    output.webp({ quality: options.quality });
 
   const outputBuffer = await output.toBuffer();
 
   const outputAttachment = new MessageAttachment(outputBuffer, `${options.name}.${options.format}` ?? `OUTPUT.${options.format}`);
 
+  const newMetadata = await getImageMetadata(outputBuffer);
+
+  const statisticsEmbed = new MessageEmbed()
+    .setTitle('Estadísticas de la conversión')
+    .setDescription(`**Peso (KB)**: **${oldMetadata.size / 1000}** -> **${newMetadata.size / 1000}**, **(${calculatePercentage(oldMetadata.size, newMetadata.size)})**`
+      + `\n**Dimensiones LargoxAncho (px)**: **${oldMetadata.width}x${oldMetadata.height}** -> **${newMetadata.width}x${newMetadata.height}**`
+      + `\n**Formato**: **${oldMetadata.format}** -> **${newMetadata.format}**`)
+    .setColor(Util.resolveColor('BLUE'))
+    .setImage(`attachment://${options.name}.${options.format}`)
+    .setTimestamp();
+
   return await interaction.editReply({
     content: '¡Aquí está tu nueva imagen!\nDebes descargarla antes de eliminar este mensaje. Si copias la URL de la imagen y borras este mensaje, la imagen desaparecerá y la URL no funcionará.',
-    files: [outputAttachment]
+    files: [outputAttachment],
+    embeds: [statisticsEmbed]
   })
+}
+
+async function getImageMetadata(buffer) {
+  return await Sharp(buffer).metadata();
+}
+
+/**
+ * 
+ * @param {Number} oldSize El tamaño original del archivo
+ * @param {Number} newSize El nuevo tamaño del archivo
+ */
+function calculatePercentage(oldSize, newSize) {
+
+  const base = 100;
+  const firstOperation = oldSize / newSize;
+  const secondOperation = 1 / firstOperation;
+  const thirdOperation = secondOperation * 100;
+  const result = 100 - thirdOperation;
+
+  if (result <= 0)
+    return `${Math.abs(thirdOperation).toFixed(2) - 100}% más peso`
+  else
+    return `${Math.abs(result).toFixed(2)}% menos peso`
 }
--- Listeners/Interactions/Commands/cmd-image.js
+++ Listeners/Interactions/Commands/cmd-image.js
@@ -22,23 +22,23 @@ module.exports = {
           url.setName('url')
             .setDescription('La url de la imágen en caso de que sea una imagen online')
         )
+        .addStringOption(outputName =>
+          outputName.setName('nombre')
+            .setDescription('El nombre final del archivo')
+        )
         .addIntegerOption(width => width.setName('largo').setDescription('Nuevo largo de la imagen en pixeles (MAX: 2048, MIN: 8)').setMaxValue(2048).setMinValue(8))
         .addIntegerOption(height => height.setName('altura').setDescription('Nueva altura de la imagen en pixeles (MAX: 2048, MIN: 8)').setMaxValue(2048).setMinValue(8))
         .addStringOption(outputFormat =>
           outputFormat.setName('formato')
             .setDescription('El formato final de la imagen')
             .setChoices(
-              { name: 'JPG', value: 'jpg' },
+              { name: 'JPG / JPEG', value: 'jpeg' },
               { name: 'PNG', value: 'png' },
               { name: 'WEBP', value: 'webp' },
               { name: 'GIF', value: 'gif' }
             )
         )
         .addIntegerOption(quality => quality.setName('calidad').setDescription('Calidad de la compresión (1 - 100)').setMinValue(1).setMaxValue(100))
-        .addStringOption(outputName =>
-          outputName.setName('nombre')
-            .setDescription('El nombre final del archivo')
-        )
     )
   ,
   isGlobal: false,
