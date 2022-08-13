7c8ede590209601afca114cc7f1aa2b95bc6b9b8
Shompi <shompire02@gmail.com>
2022-08-13 06:32:40 -0400

    Meh, i dont liked the implementation of this

--- Listeners/Interactions/Commands/SubCommands/roles-assign.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RolesAssign = void 0;
-const RolesAssign = async (interaction) => {
-    return;
-};
-exports.RolesAssign = RolesAssign;
--- Listeners/Interactions/Commands/SubCommands/roles-remove.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RolesRemove = void 0;
-const RolesRemove = (interaction) => {
-    return;
-};
-exports.RolesRemove = RolesRemove;
