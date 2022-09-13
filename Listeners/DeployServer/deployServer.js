0d3e1c4334b9026bcaf4ba7a8eb786637663d20c
Shompi <shompire02@gmail.com>
2022-09-12 22:11:22 -0300

    fuyumi api, fastify, lel

--- Listeners/DeployServer/deployServer.js
+++ Listeners/DeployServer/deployServer.js
@@ -1,41 +1,34 @@
-const { Listener } = require('discord-akairo');
-const http = require('node:http');
-const { Fuyumi } = require('./Routes/Api/fuyumi');
-class DeployServer extends Listener {
-  constructor() {
-    super('deployServer', {
-      emitter: 'client',
-      event: 'deployServer'
-    });
-  }
-  /**
-   * 
-   * @param {import("../../Classes/Client")} client 
-   */
-  async exec(client) {
-    console.log("Starting Fuyumi stats Api...");
-
-    const server = http.createServer();
-
-    server.listen(80);
-
-    console.log("Stats api listening on port 80");
-
-    server.on('request', async (request, response) => {
-      console.log("Received a request on route:" + request.url);
-
-      if (request.url === '/') {
-        return response.writeHead(200).end('Holiiii')
-
-      } else if (request.url === '/api/fuyumi') {
-        return await Fuyumi(request, response, client);
-
-      } else {
-        return response.writeHead(404).end('Holi');
-      }
-
-    });
-  }
-}
-
-module.exports = DeployServer;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_akairo_1 = require("discord-akairo");
+const stats_1 = require("./Routes/stats");
+const fastify_1 = __importDefault(require("fastify"));
+let deployed = false;
+const SERVER = (0, fastify_1.default)();
+const PORT = 2289;
+module.exports = class DeployServer extends discord_akairo_1.Listener {
+    constructor() {
+        super('deployServer', {
+            emitter: 'client',
+            event: 'deployServer',
+        });
+    }
+    async exec(client) {
+        if (deployed)
+            return console.log("Fastify api already deployed. Ignoring...");
+        console.log("Starting fastify server on port" + PORT);
+        // Route declaration
+        console.log("Creating route /fuyumi/stats...");
+        SERVER.get('/fuyumi/stats', async (req, reply) => (0, stats_1.StatsRequestHandler)(req, reply, client));
+        console.log("OK");
+        // END Route declaration
+        const SUCCESS = await SERVER.listen({ port: PORT }).catch(err => console.error(err));
+        if (!SUCCESS)
+            return console.log("Ocurrió un error al intentar iniciar el servidor.");
+        deployed = true;
+        console.log("Server initiated!", SUCCESS);
+    }
+};
